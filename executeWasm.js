fc559432f32940b26b6355948f39a476ff76218e
KeeganBeuthin <kdbeuthin@gmail.com>
2024-06-19 14:41:21 +0200

    rust functional

--- executeWasm.js
+++ executeWasm.js
@@ -1,3 +1,4 @@
+//executeWasm.js
 const fs = require("fs");
 const { promisify } = require("util");
 
@@ -5,39 +6,74 @@ async function executeWasmFile(filePath) {
   try {
     const wasmBuffer = await promisify(fs.readFile)(filePath);
     const importObject = {
-      gojs: {
-        "runtime.sleepTicks": () => {},
-        "runtime.ticks": () => {},
-        "syscall/js.valueGet": () => {},
-        "syscall/js.valuePrepareString": () => {},
-        "syscall/js.valueLoadString": () => {},
-        "syscall/js.finalizeRef": () => {},
-        "syscall/js.stringVal": () => {},
-        "syscall/js.valueSet": () => {},
-        "syscall/js.valueNew": () => {},
-        "syscall/js.valueLength": () => {},
-        "syscall/js.valueIndex": () => {},
-        "syscall/js.valueCall": () => {},
+      wbg: {
+        __wbg_new_abda76e883ba8a5f: () => {},
+        __wbg_stack_658279fe44541cf6: () => {},
+        __wbg_error_f851667af71bcfc6: () => {},
+        __wbindgen_object_drop_ref: () => {}
       },
       wasi_snapshot_preview1: {
+        args_get: () => {},
+        args_sizes_get: () => {},
+        environ_get: () => {},
+        environ_sizes_get: () => {},
+        clock_res_get: () => {},
+        clock_time_get: () => {},
+        fd_advise: () => {},
+        fd_close: () => {},
+        fd_datasync: () => {},
+        fd_fdstat_get: () => {},
+        fd_fdstat_set_flags: () => {},
+        fd_filestat_get: () => {},
+        fd_filestat_set_size: () => {},
+        fd_filestat_set_times: () => {},
+        fd_pread: () => {},
+        fd_prestat_get: () => {},
+        fd_prestat_dir_name: () => {},
+        fd_pwrite: () => {},
+        fd_read: () => {},
+        fd_readdir: () => {},
+        fd_seek: () => {},
+        fd_sync: () => {},
+        fd_tell: () => {},
         fd_write: () => {},
+        path_create_directory: () => {},
+        path_filestat_get: () => {},
+        path_filestat_set_times: () => {},
+        path_link: () => {},
+        path_open: () => {},
+        path_readlink: () => {},
+        path_remove_directory: () => {},
+        path_rename: () => {},
+        path_symlink: () => {},
+        path_unlink_file: () => {},
+        poll_oneoff: () => {},
+        proc_exit: () => {},
+        sched_yield: () => {},
+        random_get: () => {},
+        sock_accept: () => {},
+        sock_recv: () => {},
+        sock_send: () => {},
+        sock_shutdown: () => {},
       },
     };
+
+
     const { instance } = await WebAssembly.instantiate(wasmBuffer, importObject);
-    console.log(instance.exports.executeDebitLeg(60.1));
+    console.log(instance.exports.http_request());
 
     // Test executeCreditLeg function
     // Test executeCreditLeg function
-    const executeCreditLeg = instance.exports.executeCreditLeg;
+    const executeCreditLeg = instance.exports.execute_credit_leg
     if (typeof executeCreditLeg === "function") {
-      const result = executeCreditLeg(100.0);
+      const result = executeCreditLeg();
       console.log(result);
     } else {
       console.log("executeCreditLeg function not found.");
     }
 
     // Test executeDebitLeg function
-    const executeDebitLeg = instance.exports.executeDebitLeg;
+    const executeDebitLeg = instance.exports.execute_debit_leg;
     if (typeof executeDebitLeg === "function") {
       const result = executeDebitLeg(50.0);
       console.log(result);
@@ -46,7 +82,7 @@ async function executeWasmFile(filePath) {
     }
 
     // Test httpRequest function
-    const httpRequest = instance.exports.httpRequest;
+    const httpRequest = instance.exports.http_request;
     if (typeof httpRequest === "function") {
       const result = httpRequest("https://jsonplaceholder.typicode.com/posts/1");
       console.log(result);
--- testing-files/rust/src/lib.rs
+++ testing-files/rust/src/lib.rs
@@ -1,11 +1,27 @@
-use wasm_bindgen::prelude::*; // ¹
+use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen] // ²
-pub fn greeter(name: &str) -> Result<String, JsError /* ³ */> {
-    Ok(format!("Hello {name}!"))
+// Function to simulate crediting an amount to an account
+#[wasm_bindgen]
+pub fn execute_credit_leg(amount: f64, account: &str) -> String {
+    format!("Crediting {} to account {}", amount, account)
 }
 
-#[wasm_bindgen(start)] // ⁴
-fn main() {
-    console_error_panic_hook::set_once(); // ⁵
+// Function to simulate debiting an amount from an account
+#[wasm_bindgen]
+pub fn execute_debit_leg(amount: f64, account: &str) -> String {
+    format!("Debiting {} from account {}", amount, account)
+}
+
+// Simple function to add two numbers, simulating an HTTP request
+#[wasm_bindgen]
+pub fn http_request(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// The start function is automatically called when the WASM module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() {
+    // Set up a panic hook to provide better error messages if something goes wrong
+    // This is especially useful during development
+    console_error_panic_hook::set_once();
 }
